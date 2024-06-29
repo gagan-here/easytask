7a83c5dce1d69ca606351df6f1e1150cf28ecd0f
Gagan Basnet <chatwidmeah@gmail.com>
2024-06-29 18:26:17 +0545

    implemented output function for emitting data to parent components

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, computed, input } from '@angular/core';
+import { Component, EventEmitter, Input, Output, computed, input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,8 +7,10 @@ import { Component, Input, computed, input } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  @Input({required: true}) id!:string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
+  @Output() select = new EventEmitter()
   
 
   get imagePath() {
@@ -16,6 +18,6 @@ export class UserComponent {
   }
 
   onSelectUser() {
-    
+    this.select.emit(this.id);
   }
 }
